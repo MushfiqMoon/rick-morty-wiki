91c83e8d74d039d613ad452eec7b90c2ecc95c91
MushfiqMoon <mushfiq.moon013@google.com>
2022-06-29 01:43:03 +0600

    project setup v2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
 
 function App() {
   return (
     <div className="App">
-        rick morty wiki
+      <h1 className="text-center ubuntu">rick morty <span className="text-primary">wiki</span></h1>
+        
     </div>
   );
 }
