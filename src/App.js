698ca84f0eb1d8b52e22bed53774f1ae4f4f8ee8
MushfiqMoon <mushfiq.moon013@google.com>
2022-06-29 03:17:54 +0600

    structure part-one

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
+import React, { useState, useEffect } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import Cards from "./components/Cards/Cards";
+import Filters from "./components/Filters/Filters";
 
 function App() {
+  let [pageNumber, setPageNumber] = useState(1);
+  const [featchedData, setFeatchedData] = useState([]);
+  let { info , results} = featchedData;
+  console.log(info);
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+  useEffect(() => {
+    (async function() {
+      let data = await fetch(api).then((res) =>res.json())
+      setFeatchedData(data)
+      
+    })()
+  
+  }, [api])
+  
+  // let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}&status=${status}&gender=${gender}&species=${species}`;
   return (
     <div className="App">
-      <h1 className="text-center ubuntu">rick morty <span className="text-primary">wiki</span></h1>
-        
+      <h1 className="text-center ubuntu">Rick & Morty <span className="text-primary">wiki</span></h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters />
+          </div>
+          <div className="col-8">
+            <div className="row">
+              <Cards />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
